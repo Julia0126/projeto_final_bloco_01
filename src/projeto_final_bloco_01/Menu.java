7fbbeeeabdd7d56db75b7d7feeebc9705eb4aa83
Julia <jrhuliafy@gmail.com>
2024-06-20 10:24:52 -0300

    Criação do Menu

---
+++ src/projeto_final_bloco_01/Menu.java
@@ -0,0 +1,69 @@
+package projeto_final_bloco_01;
+
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		int opcao;
+		
+		while (true) {
+
+			System.out.println("________________________♥____________________________");
+			System.out.println("                                                     ");
+			System.out.println("                E-commerce JavaBeauty                ");
+			System.out.println("                                                     ");
+			System.out.println("_____________________________________________________");
+			System.out.println("                                                     ");
+			System.out.println("            1 - Cadastrar produto                    ");
+			System.out.println("            2 - Listar todos os produtos             ");
+			System.out.println("            3 - Buscar produto por número            ");
+			System.out.println("            4 - Atualizar dados do produto           ");
+			System.out.println("            5 - Apagar produto                       ");
+			System.out.println("            6 - Sair                                 ");
+			System.out.println("                                                     ");
+			System.out.println("________________________♥____________________________");
+			System.out.println("Digite a opção desejada:                             ");
+			System.out.println("                                                     ");
+
+			opcao = leia.nextInt();
+
+			if (opcao == 6) {
+				System.out.println("\n JavaBeauty ♥ Beleza codificada para você!");
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+				case 1:
+					System.out.println("Cadastrar produto\n\n");
+
+					break;
+				case 2:
+					System.out.println("Listar todos os produtos\n\n");
+
+					break;
+				case 3:
+					System.out.println("Consultar dados do produto - por número\n\n");
+
+					break;
+				case 4:
+					System.out.println("Atualizar dados do produto\n\n");
+
+					break;
+				case 5:
+					System.out.println("Apagar o produto\n\n");
+
+					break;
+				default:
+					System.out.println("\nOpção Inválida!\n");
+					break;
+			}
+		}
+	}
+		
+
+	}
